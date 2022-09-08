27b74a6303162add146bc41c3b1121e33fd98a06
AfricanBongo <tilormta@outlook.com>
2022-09-08 04:29:07 +0200

    Splash screen tweak

--- lib/ui/splashscreen.dart
+++ lib/ui/splashscreen.dart
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 3),
+    Timer(const Duration(seconds: 8),
         () => Navigator.of(context).push(_createRoute()));
   }
 
